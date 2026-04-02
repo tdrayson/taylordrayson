d69f73ec65df4a54e14634d99b07a4aeca7cf549
tdrayson <taylor@drayson.co.uk>
2026-04-02 19:57:45 +0100

    refactor: update home page to use list elements for timeline entries

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,19 +10,26 @@
             @endphp
 
             @if ($currentDate !== $lastDate)
+                @if ($lastDate !== null)
+                    </ul>
+                @endif
                 <h2>{{ $entry->occurred_at->format('l j F Y') }}</h2>
+                <ul>
                 @php $lastDate = $currentDate; @endphp
             @endif
 
-            <p>
+            <li>
                 <strong>{{ $card['title'] }}</strong>
                 @if ($card['subtitle'])
                     &mdash; {{ $card['subtitle'] }}
                 @endif
                 <em>({{ $card['type'] }})</em>
-            </p>
+            </li>
         @endif
     @endforeach
+    @if ($lastDate !== null)
+        </ul>
+    @endif
 
     <hr>
     @if ($entries->hasPages())
