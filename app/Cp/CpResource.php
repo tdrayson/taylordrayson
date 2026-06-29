a948730f1be6ef2ee2706ccb89e28efc658fd764
tdrayson <taylor@drayson.co.uk>
2026-06-29 17:48:46 +0100

    fix: reject relation fields from default index columns

--- app/Cp/CpResource.php
+++ app/Cp/CpResource.php
@@ -141,7 +141,7 @@ abstract class CpResource
     public function columns(): array
     {
         return collect($this->fields())
-            ->reject(fn (array $field): bool => in_array($field['type'], ['textarea', 'json', 'editor', 'group', 'keyvalue'], true))
+            ->reject(fn (array $field): bool => in_array($field['type'], ['textarea', 'json', 'editor', 'group', 'keyvalue', 'relation'], true))
             ->take(4)
             ->map(fn (array $field): array => ['key' => $field['key'], 'label' => $field['label']])
             ->values()
